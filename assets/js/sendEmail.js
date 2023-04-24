4111743629cb7836c971d962baeea5828337ea3c
Tanise Carvalho <carvalhotanise@gmail.com>
2023-04-24 17:50:43 +0000

    implement emailjs integration on contact page

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,17 @@
+function sendMail(contactForm) {
+  emailjs.send("service_aju9e3j","project_request", {
+    "from_name": contactForm.name.value,
+    "from_email": contactForm.emailaddress.value,
+    "project_request": contactForm.projectsummary.value
+  })
+  .then(
+    function(response) {
+      console.log("SUCCESS", response);
+    },
+    function(error) {
+      console.log("FAILED", error);
+    }
+  );
+
+  return false;
+}
